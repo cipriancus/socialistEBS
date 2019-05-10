3f8fb4552b32b826585d9fa62f3c735fbfdbd21e
Ciprian Cusmuliuc <ciprian.cusmuliuc@nttdata.ro>
2019-05-10 09:32:00 +0300

    implemented some broker communications

--- broker/src/main/java/com/ebs/broker/configuration/RestTemplateBean.java
+++ broker/src/main/java/com/ebs/broker/configuration/RestTemplateBean.java
@@ -1,12 +1,15 @@
 package com.ebs.broker.configuration;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateBean {
   @Bean
+  @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
   public RestTemplate restTemplateBean() {
     return new RestTemplate();
   }
--- broker/src/main/java/com/ebs/broker/service/BrokerCommunicationService.java
+++ broker/src/main/java/com/ebs/broker/service/BrokerCommunicationService.java
@@ -2,25 +2,49 @@ package com.ebs.broker.service;
 
 import com.google.common.base.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
 public class BrokerCommunicationService {
 
-  private List<String> knownBrokers;
+  @Value("server.self-identity")
+  private String myIp;
+
+  @Value("server.broker-list")
+  private String brokerStringList;
 
   @Autowired private RestTemplate restTemplate;
+  private List<String> knownBrokers;
 
   public BrokerCommunicationService() {
     knownBrokers = new ArrayList<>();
+    populateKnownBrokers();
+  }
+
+  @Scheduled(cron = "* 1 * * * *")
+  public void pingKnownBrokers() {
+    Iterator<String> iterator = knownBrokers.iterator();
+    while (iterator.hasNext()) {
+      String ip = iterator.next();
+      if (ping(ip) != true) {
+        iterator.remove();
+      } else {
+        System.out.println("Broker has responded " + ip);
+      }
+    }
   }
 
   public boolean joinNetwork(String ipAddress) {
-    if (ping(ipAddress) == true) {
+    if (ping(ipAddress) == true && isNotInKnownBrokers(ipAddress) == true) {
       knownBrokers.add(ipAddress);
       return true;
     }
@@ -39,6 +63,42 @@ public class BrokerCommunicationService {
     } catch (Exception e) {
       // doesn't matter what exception
       return false;
+    }
+    return true;
+  }
+
+  public boolean sendJoinNetworkRequest(String ipAddress) {
+    try {
+      ResponseEntity<String> response = restTemplate.postForEntity(ipAddress, null, String.class);
+      if (response.getStatusCode() == HttpStatus.OK) {
+        return true;
+      }
+    } catch (RestClientException e) {
+      // doesn't matter what exception, but it's dead
+      return false;
+    } catch (Exception e) {
+      // doesn't matter what exception
+      return false;
+    }
+    return true;
+  }
+
+  private void populateKnownBrokers() {
+    String brokerList[] = brokerStringList.split(",");
+    for (String iterator : brokerList) {
+      if (!iterator.equals(myIp)
+          && isNotInKnownBrokers(iterator)
+          && sendJoinNetworkRequest(iterator)) {
+        knownBrokers.add(iterator);
+      }
+    }
+  }
+
+  private boolean isNotInKnownBrokers(String ipAddress) {
+    for (String iterator : knownBrokers) {
+      if (iterator.equals(ipAddress)) {
+        return false;
+      }
     }
     return true;
   }
